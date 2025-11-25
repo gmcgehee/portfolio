4f993db06c27344291afb632746aaff4cd40ec04
gmcgehee <geist.mcgehee@eagles.cui.edu>
2025-11-25 12:18:41 -0800

    Added github api functionality, set groundwork for interface with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProjectCard from './components/card';
 import './App.css';
 import './index.css';
@@ -6,23 +6,58 @@ import getCards from './scripts/getCards';
 
 
 export default function App() {
+
   const [lifeSection, setLifeSection] = useState(0) // we want three sections: projects, professional experience, 
-  //const repos = //getCards("gmcgehee"); // comment out when not in use, rate-limited to 60 req/hour
+  const [repos, setRepos] = useState<string[]>([])
+
+  useEffect(() => { // this allows me to get cards and set them to an actual global value
+    let currRepos;
+
+    try {
+      currRepos = localStorage.getItem('repos')
+
+      const repoJson = currRepos === null ? '{}' : JSON.parse(currRepos)
+      
+      if (repoJson.length !== JSON.parse('{}')) {
+        setRepos(repoJson)
+        console.log(repoJson)
+      }
+    }
+
+    catch (error) {
+      console.log(error)
+    }
+
+    async function load() {
+      const cards = await getCards("gmcgehee"); // waits for the repos to respond
+      console.log('Fetching repos...')
+      setRepos(cards) // uses the data I have and puts it to my global value, thanks!
+    }
+
+    if (repos?.length == 0) {
+      load()
+    }
+
+  }, [])
+
+  useEffect(() => {
+    const storeRepos: string = JSON.stringify(repos)
+    localStorage.setItem('repos', storeRepos)
+
+  }, [repos])
+
   return (
     <div id="full-screen" className="flex flex-style-rows w-[100vw] h-[100vh]">
       {/* Page Title */}
-    <div id="left" className="border w-[50%]">
-      <h1>Portfolio</h1>
+      <div id="left" className="border w-[50%]">
+        <h1>Portfolio</h1>
 
       </div>
-      {/* max-h-screen overflow-y-auto */}
       <div id="right" className='border w-[50%] h-[100%]'>
         <div id="cards" className="grid grid-col gap-[30vh] h-[100%] justify-center pt-[20%] pb-[20%] overflow-auto">
-          
-            <ProjectCard title="DES Encryption" description="Low-level implementation of Triple DES including block cipher modes ECB, CBC, and OFB." imageUrl="https://www.kaspersky.com/content/en-global/images/repository/isc/2021/encryption-1.jpg">
-            </ProjectCard>
-             {/* etc. */}
-          
+
+          {repos}
+
         </div>
       </div>
 
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,16 +3,18 @@ import React from "react";
 export interface cardParts { 
     title: string;
     description: string;
+    language: string;
     imageUrl: string;
 }
 
-function ProjectCard({ title, description, imageUrl }: cardParts) {
+function ProjectCard(title: string, description: string, language: string, imageUrl: string, id: string) {
     return (
-        <div className="border p-[2vh] w-[35vw] h-[60vh] rounded-s">
-            <div><h1 className="">{title}</h1>  {/* Fix the auto-sizing on this; it should look good no matter viewport */}
+        <div className="border p-[2vh] w-[35vw] h-[60vh] rounded-s" key={id}>
+            <p>{title}</p>  {/* Fix the auto-sizing on this; it should look good no matter the viewport */}
             <p className="">{description}</p>
+            <p>{language}</p>
             <img src={imageUrl} className="h-[50%] w-[50%]" />
-            </div>
+            
         </div>
     );
 }
